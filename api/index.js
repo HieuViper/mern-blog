645e7b1cfe6741a8e93c8916827093a4a58525c1
Phan Minh Hiếu <51900741@student.tdtu.edu.vn>
2024-04-02 15:10:12 +0700

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => console.log("Server running on port 3000!"));
